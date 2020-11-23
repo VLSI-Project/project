93470df4e90c0577855aecdbb9766808f5f99d5e
mitrakanishka9732 <ankhsaki@gmail.com>
2020-11-23 13:14:20 -0600

    Add files via upload

--- lut3.v
+++ lut3.v
@@ -1,18 +1,43 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-// 3-input lookup table
 module lut3(
-  input  shift_clk,
-  input  shift_en,
-  input  shift_i,
-  output shift_o,  
 
-  input  a,
-  input  b,
-  input  c,
-  output y);
-
-  // logic to shift data in/out
-  // logic to generate y
-
-endmodule
+   input shift_clk, 
+   input shift_en, 
+   input shift_i, 
+   output shift_o, 
+   
+   input a, 
+   input b, 
+   input c, 
+   output reg y); 
+   
+   //assign the output based on the select lines 
+   always @(posedge shift_clk) begin 
+   
+   if (shift_en) begin //if shift is enabled, assign the output 
+   case(a & b & c)
+   
+   3'b000: y = shift_i; 
+   3'b001: y = shift_i;
+   3'b010: y = shift_i;
+   3'b011: y = shift_i;
+   3'b100: y = shift_i;
+   3'b101: y = shift_i;
+   3'b110: y = shift_i;
+   3'b111: y = shift_i;
+   
+   default: y = 0; 
+   endcase 
+   
+   end 
+   else begin 
+   
+      y = 0; 
+   end 
+   
+   
+   end 
+   
+   
+endmodule 
